452cb58ad961534779b72c519c9cbd5386422fb2
SAN <florescencebit@gmail.com>
2025-11-25 17:31:04 +0530

    Refactor Install.sh for improved installation process
    Updated the installation script to improve clarity and functionality, including changes to disk partitioning, mounting, user creation, and theme installation.

--- Install.sh
+++ Install.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 set -e
 
+# --- CONFIGURATION ---
+DISK="/dev/nvme0n1"
 SUMMARY="/root/installation-summary.txt"
+
+# --- START ---
 echo "=== Arch Linux Installation Summary ===" > $SUMMARY
 echo "Start Time: $(date)" >> $SUMMARY
 
-DISK=/dev/nvme0n1
-
-echo ">>> Partitioning Disk"
+echo ">>> Partitioning Disk ($DISK)"
+# Wipes the disk and creates partitions
+# p1: EFI (500M), p2: Root (150G), p3: Home (Remaining)
 {
 sgdisk -Z $DISK
 sgdisk -n 1:0:+500M -t 1:ef00 $DISK
@@ -22,12 +26,14 @@ mkfs.ext4 ${DISK}p2
 mkfs.ext4 ${DISK}p3
 } >> $SUMMARY
 
-echo ">>> Mounting"
+echo ">>> Mounting File Systems"
+# NOTE: Mounting EFI to /boot is the standard for systemd-boot
+# so it can find the kernel images automatically.
 {
 mount ${DISK}p2 /mnt
-mkdir /mnt/efi
-mkdir /mnt/home
-mount ${DISK}p1 /mnt/efi
+mkdir -p /mnt/boot
+mkdir -p /mnt/home
+mount ${DISK}p1 /mnt/boot
 mount ${DISK}p3 /mnt/home
 } >> $SUMMARY
 
@@ -38,7 +44,8 @@ echo ">>> Generating fstab"
 genfstab -U /mnt >> /mnt/etc/fstab
 
 echo ">>> Entering Chroot"
-arch-chroot /mnt /bin/bash << "EOF"
+# IMPORTANT: We use unquoted EOF so we can escape variables manually
+arch-chroot /mnt /bin/bash << EOF
 
 SUMMARY="/root/installation-summary.txt"
 
@@ -51,6 +58,9 @@ sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 locale-gen
 echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
+# Fixes the vconsole error from your screenshot
+echo "KEYMAP=us" > /etc/vconsole.conf
+
 echo ">>> Hostname setup" >> \$SUMMARY
 echo "arch" > /etc/hostname
 
@@ -59,10 +69,8 @@ systemctl enable NetworkManager
 
 echo ">>> Creating user 'san'" >> \$SUMMARY
 useradd -m -G wheel san
-
-echo "san:san" | chpasswd   # Auto password set
-
-# Allow sudo
+echo "san:san" | chpasswd
+# Allow sudo for wheel group
 sed -i 's/# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
 
 echo ">>> Installing GNOME + Extensions" >> \$SUMMARY
@@ -74,20 +82,26 @@ systemctl enable gdm
 echo ">>> Installing systemd-boot" >> \$SUMMARY
 bootctl install
 
-ROOT_UUID=\$(blkid -s UUID -o value /dev/nvme0n1p2)
+# Capture UUID for the root partition
+ROOT_UUID=\$(blkid -s UUID -o value ${DISK}p2)
 
-cat << BOOT > /boot/loader/entries/arch.conf
+# Create Loader Entry
+cat << BOOTCONF > /boot/loader/entries/arch.conf
 title   Arch Linux
 linux   /vmlinuz-linux
 initrd  /intel-ucode.img
 initrd  /initramfs-linux.img
 options root=UUID=\$ROOT_UUID rw
-BOOT
+BOOTCONF
 
+# Set Default Loader
 echo "default arch" > /boot/loader/loader.conf
+echo "timeout 3" >> /boot/loader/loader.conf
 
 echo ">>> Preparing macOS theme autoinstall" >> \$SUMMARY
 mkdir -p /home/san/.config/autostart
+
+# Create the install script that runs on first login
 cat << THEMESCRIPT > /home/san/install-theme.sh
 #!/bin/bash
 cd ~
@@ -103,6 +117,36 @@ cd ../WhiteSur-icon-theme
 
 cd ../WhiteSur-cursors
 sudo ./install.sh
+
+gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-light-blue"
+gsettings set org.gnome.desktop.interface icon-theme "WhiteSur"
+gsettings set org.gnome.desktop.interface cursor-theme "WhiteSur-cursors"
+
+# Self-destruct this script after running
+rm -f ~/install-theme.sh
+rm -f ~/.config/autostart/theme.desktop
+THEMESCRIPT
+
+chmod +x /home/san/install-theme.sh
+chown san:san /home/san/install-theme.sh
+
+# Create Autostart entry
+cat << AUTOSTART > /home/san/.config/autostart/theme.desktop
+[Desktop Entry]
+Type=Application
+Exec=/home/san/install-theme.sh
+X-GNOME-Autostart-enabled=true
+Name=Theme Installer
+AUTOSTART
+
+chown san:san /home/san/.config/autostart/theme.desktop
+
+echo ">>> Chroot Configuration Done" >> \$SUMMARY
+EOF
+
+echo "End Time: $(date)" >> $SUMMARY
+echo ">>> INSTALLATION COMPLETE"
+echo ">>> You can now type 'reboot'"
 
 gsettings set org.gnome.desktop.interface gtk-theme "WhiteSur-light-blue"
 gsettings set org.gnome.desktop.interface icon-theme "WhiteSur"
